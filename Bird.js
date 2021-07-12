cefa75e3efa34904a90a2ca0446fe6c1f32088a3
Zarif Ahanaf <zarif.ahanaf08@gmail.com>
2021-07-12 14:47:45 -0400

    Made smoke behind bird and fixed a bug

--- Bird.js
+++ Bird.js
@@ -2,12 +2,20 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.image1 = loadImage("sprites/smoke.png");
+    this.array = []
   }
 
   display() {
-    //this.body.position.x = mouseX;
+    if(this.body.velocity.x>10&&this.body.position.x>200){
+    var pos = [this.body.position.x, this.body.position.y]
+    this.array.push(pos)
+    }
+    for(var i = 0; i<this.array.length; i++){
+      image(this.image1, this.array[i][0], this.array[i][1])
+    }
     //this.body.position.y = mouseY;
-
+    console.log(this.array)
     super.display();
   }
 }
